10cf01f15bc07b997c0568f35bf3c03a1e5267e6
Rohan <rohankanchana@github.com>
2020-04-13 14:12:21 -0400

    template

--- template.cpp
+++ template.cpp
@@ -51,6 +51,16 @@ struct custom_hash {
     }
 };
 
+struct pair_hash {
+    template <class T1, class T2>
+    std::size_t operator () (std::pair<T1, T2> const &pair) const {
+        std::size_t h1 = std::hash<T1>()(pair.first);
+        std::size_t h2 = std::hash<T2>()(pair.second);
+        
+        return h1 ^ h2;
+    }
+};
+
 //unordered_set<int, custom_hash> s;
 
 void set_IO(string name) {
